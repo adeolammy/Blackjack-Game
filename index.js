bbbf8b0656c5551ed4ab73bba957031cf563f6ed
adeolammy <adeolammy2020@gmail.com>
2024-05-06 11:59:01 +0100

    Added other logics to the game

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-let player = {
-    name: "Per",
-    chips: 200
-}
 
 let cards = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+// let inputName = document.getElementById("name").value;
+// let inputAmount = document.getElementById('amount').value;
 
-playerEl.textContent = player.name + ": $" + player.chips
+
+
+// let player = {
+//     name: inputName,
+//     chips: inputAmount
+// }
+
+
+
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
@@ -26,6 +32,21 @@ function getRandomCard() {
     }
 }
 
+function name_Amount_field() {
+  
+    let inputName = document.getElementById("name").value;
+    let inputAmount = document.getElementById('amount').value;
+
+    document.getElementById('inputFields').style.display = 'none';
+
+    let playerInfo =  `Congratulations ${inputName}, you won $ ${inputAmount * 12}`
+  
+    let playerEl = document.getElementById('player-el');
+    playerEl.textContent = playerInfo;
+
+}
+
+
 function startGame() {
     isAlive = true
     let firstCard = getRandomCard()
@@ -33,9 +54,12 @@ function startGame() {
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
     renderGame()
+    
+
 }
 
 function renderGame() {
+    document.getElementById('inputFields').style.display = 'none';
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
@@ -47,6 +71,7 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        name_Amount_field();
     } else {
         message = "You're out of the game!"
         isAlive = false
